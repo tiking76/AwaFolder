0b13c8fe11116e655c764f0081fb7aa8e403d805
tiking76 <716yosiki1999@gmail.com>
2022-09-24 09:44:25 +0900

    Initial Commit

---
+++ PhotoConect/PhotoConectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoConectApp.swift
+//  PhotoConect
+//
+//  Created by tiking on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoConectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
